121a8c6cce380968d478d298f67b6b3f67547486
jcfpascua <jcfpascua@gmail.com>
2024-10-16 14:48:32 +0800

    Project files

---
+++ Assets/Scripts/LogicScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    public Text winText;
+    public Button playAgainButton;
+
+    void Start()
+    {
+        winText.gameObject.SetActive(false);
+        playAgainButton.gameObject.SetActive(false);
+    }
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore += scoreToAdd;
+        scoreText.text = playerScore.ToString();
+
+        if (playerScore == 5)
+        {
+            Time.timeScale = 0f;
+            winText.text = "You Win!";  
+            winText.gameObject.SetActive(true);
+            playAgainButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void gameOver()
+    {
+        Time.timeScale = 0f;
+        gameOverScreen.SetActive(true);
+    }
+}
+
